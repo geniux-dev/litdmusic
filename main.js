429e083d4183d7c801b40b4be3b252c583ff6a52
Evidence Maphutha <theevilgeniux@gmail.com>
2019-06-17 21:14:24 -0700

    New pages added

---
+++ main.js
@@ -0,0 +1,38 @@
+// Get the button that opens the modal
+var btn = document.querySelectorAll("button.btn.btnD2");
+
+// All page modals
+var modals = document.querySelectorAll('.modal');
+
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("data-target"));
+    modal.style.display = "block";
+ }
+}
+
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none"; 
+modal.pause();	  
+    }
+ }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "block";    
+     
+	 }
+    }
+	
+}
